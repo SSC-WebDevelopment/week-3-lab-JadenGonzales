0e0b403b9477f4283303c96f1a8522e5d5a00057
JadenGonzales <131292845+JadenGonzales@users.noreply.github.com>
2023-09-14 21:16:51 -0400

    added html page. completed javascript

--- dogYearsStartFile.js
+++ dogYearsStartFile.js
@@ -1,19 +1,24 @@
+//Jaden Gonzales 9/14/23
 //store the value of my current age
-const myAge
+const myAge = 20;
 
 //store the value of 2
-let earlyYears 
+let earlyYears = 2;
 earlyYears *= 10.5;
 
-//you finsih the rest!
-
 //calculate years
+let laterYears = earlyYears-2;
 
-//calc dog years
+//calc dog years of later years
 laterYears *= 4;
-console.log(earlyYears);
-console.log(laterYears);
+//console.log(earlyYears);
+//console.log(laterYears);
 
-//add years
+//add early and later years to get age in dog years
+const myAgeInDogYears = earlyYears + laterYears;
 
 //print to the console values stored in variables
+const myName = "Jaden".toLowerCase();
+//console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+const resultElement = document.getElementById("result");
+resultElement.textContent = `My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`;
